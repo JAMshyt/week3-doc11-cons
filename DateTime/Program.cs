40801132c68dfa8c7393997258aeac2d2a0fe6b2
JAMshyt <77636014+JAMshyt@users.noreply.github.com>
2022-12-16 18:22:56 +0300

    Add files via upload

---
+++ DateTime/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лаба7
+{
+    class Date
+    {
+        public DateTime date;
+        
+        public Date()
+        {
+            
+            do
+            {
+                Console.Write("Введите дату(дд.мм.гггг): ");
+                if (DateTime.TryParse(Console.ReadLine(), out date))
+                {
+                    DateTime data = new DateTime(2009,01,1);
+                    Console.WriteLine(data.ToString("d.MM.yyyy"));
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка");
+                }
+
+            }
+            while (true);
+        }
+
+        public void days()
+        {
+            DateTime now = date;
+            DateTime yesterday = now.AddDays(-1);
+            DateTime tomorrow = now.AddDays(1);
+            int daysLeft = DateTime.DaysInMonth(now.Year, now.Month) - now.Day;
+            Console.Write(" "
+                + "\nВчера: " + yesterday.ToString("dd.MM.yyyy")
+                + "\nЗавтра: " + tomorrow.ToString("dd.MM.yyyy")
+                + "\nОсталось дней до конца месяца: " + daysLeft);
+        }
+        public int leapYear
+        {
+            get
+            {
+                int year;
+                do
+                {
+                    Console.Write("Введите год:");
+                    if (Int32.TryParse(Console.ReadLine(), out year))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка");
+                    }
+                }
+                while (true);
+                return year;
+            }
+            set
+            {
+                leapYear = 0;
+            }
+        }
+        public string leap
+        {
+            get
+            {
+                string yes;
+                bool lea = DateTime.IsLeapYear(leapYear);
+                if (lea == true) { yes = "Высокосный"; } else { yes = "Не высокосный"; }
+                return yes;
+            }
+        }
+        public void knowLeap()
+        {
+            Console.Write("" + leap);
+        }
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Date d = new Date();
+                Console.WriteLine("");
+                d.days();
+                Console.WriteLine("\n");
+                try
+                {
+                    d.knowLeap();
+                    Console.WriteLine("\n");
+                }
+                catch { Console.WriteLine("год ддолжен быть >0"); }
+            }
+        }
+    }
+}
---
+++ DateTime/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DateClassTime")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DateClassTime")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8de9fb9b-5ccd-47cc-ac49-c3a6591651a3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
